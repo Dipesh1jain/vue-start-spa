37cac05317564c56f907bfb97c13991d8f7df1c6
Dipesh1jain <dipesh18jain>
2024-06-02 14:45:24 +0530

    initial commit , adding code from  index.html to vue/cli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <navbar :pages="pages"  :active-Page="activePage"
+    :nav-link-click="(index)=>activePage= index"></navbar>
 
+    <page-viewer :page="pages[activePage]"></page-viewer>
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import   NavBar  from './components/Navbar.vue';
+import    PageViewer  from './components/PageViewer.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+      NavBar,
+    PageViewer
+  },
+  data() {
+    return {
+      // with the help of active page we are creating an index value : default 0 for home page which keeps on updating onClick Event
+      // create variables here that can help to manipulate data
+      activePage: 0,
+      useDarkNavBar: true,
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      pages: [
+        {
+          link: { text: "Home", url: "index.html" },
+          pageTitle: "HOME PAGE",
+          content: "This is Home Page",
+        },
+        {
+          link: { text: "About", url: "about.html" },
+          pageTitle: "ABOUT PAGE",
+          content: "This is About Page",
+        },
+        {
+          link: { text: "Contact", url: "contact.html" },
+          pageTitle: "CONTACT PAGE",
+          content: "This is Contact Page",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav
+    :class="[
+      `navbar-${theme}`,
+      `bg-${theme}`,
+      'navbar',
+      'navbar navbar-expand-lg',
+    ]"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Vue</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lb-0">
+        <!-- how normal HTML works -add more links for  future make more list items -->
+        <!-- nowusing VUE's "v-for" derective which is just like for-in loop to create a multiple copies of list based on Links arr size  -->
+        <li v-for="(page, index) in pages" class="nav-item" :key="index">
+          <!-- in order to bind the data with 'href' we use directive "v-bind" or simply ":" before the property
+                                    this will bind the data lets say to a link , coz '{{}}' these braces handles JS code and we simply want the link for href attriubte-->
+          <!-- "v-on:" or "@ "direictive used to handle the on click or onmouseover function-->
+          <!-- prevent property is used of on click to prevent href to open a new url instead load the content in it -->
+          <!-- applying a conditional statemnt to a class using bind directive on class, here we are uplading the active property of
+                                        HTml based on the condition if activePage == index
+                                        NOT: if a property take Hyphens "- " then need to add " '' " brakects to consider it as string 
+                                        syntax ------ :class:{'propertyName=condition','propertyName2':boolean ........}-->
+          <a
+            class="nav-link"
+            :class="{ active: activePage == index }"
+            aria-current="page"
+            :href="page.link.url"
+            :title="`this link goes to ${page.link.text} page`"
+            @click.prevent="navLinkClick(index)"
+            >{{ page.link.text }}
+          </a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">
+          Toggle NavBar
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+   props: ['pages', 'activePage','navLinkClick'],
+            data(){
+                return {
+                    theme: 'light',
+                        };
+                    },
+            methods: {
+                changeTheme() {
+                    let theme = 'light'
+                    if (this.theme == 'light') {
+                        theme = 'dark'
+                    }
+                    this.theme = theme
+                }
+            }
+};
+         
+        
+</script>
