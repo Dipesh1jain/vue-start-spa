f5898ceeedacbada3d72fa631d775dbefdf438b5
Dipesh1jain <dipesh18jain>
2024-06-02 19:41:08 +0530

    Validating Forms

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container mb-3">
     <form action="">
-    <div class="mb-3">
+    <div class="row">
+    <div class="col-md-8">
+        <div class="mb-3">
       <label for="" class="form-label">
         Page Title
       </label>
@@ -11,16 +13,50 @@
        ></input>
       </div>
 
-    <div class="mb-3">
+        <div class="mb-3">
+
          <label for="" class="form-label" >Content</label>
         <textarea type="text"
             class="form-control"
             rows="5"
             v-model="content">
-        </textarea></div>
+        </textarea>
+        </div>
+        </div>
+ <div class="col">
+   <div class="mb-3">
+         <label for="" class="form-label" >Link Test</label>
+        <input type="text"
+            class="form-control"
+        
+            v-model="LinkText">
+        </input>
+    </div>
+   <div class="mb-3">
+         <label for="" class="form-label" >Link URL</label>
+        <input type="text"
+            class="form-control"
+        
+            v-model="LinkUrl">
+        </input>
+    </div>
+    <div class="row mb-3">
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox">
+    </input>
+    <label class="form-check-label" for="gridCheck1">Published</label>
+    </div>
+    
+    </div>
+ </div>
+
+
+ </div>
+
     <div class="mb-3">
         <button class="btn btn-primary"
-        @click.prevent="pageCreated({pageTitle,content})"
+        :disabled="isFormInvalid"
+        @click.prevent="submitForm"
         >Create Page
         </button></div>
     </form>
@@ -29,10 +65,31 @@
 <script>
 export default{
     props:['pageCreated'],
+    computed:{
+isFormInvalid(){
+return !this.pageTitle || !this.content || !this.LinkText || !this.LinkUrl
+}
+    },
     data(){
         return{
             pageTitle:'',
-            content:''
+            content:'',
+            LinkText:'',
+            LinkUrl:'',
+        }
+    },
+    methods:{
+        submitForm(){
+if(!this.pageTitle || !this.content || !this.LinkText || !this.LinkUrl){
+    alert('please fill the form')
+    return;
+}
+this.pageCreated({
+    pageTitle:this.pageTitle,
+    content:this.content,
+    LinkText:this.LinkText,
+    LinkUrl:this.LinkUrl,
+})
         }
     }
 }
